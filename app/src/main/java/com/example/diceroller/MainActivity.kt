5e0d11e443c36b81fe1e36ab350e251d1c5eba45
shradd-hriyamD <54763889+shradd-hriyamD@users.noreply.github.com>
2019-11-27 00:17:05 +0530

    added to the code
    made two dice roller and also an option for getting the sum of two outputs of the dice rollers .

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,19 +9,31 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var randomInt:Int = 0
+    var randomInt1:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val diceButton : Button = findViewById(R.id.button)
-
+        val diceButton2 : Button = findViewById(R.id.button2)
+        val diceTotal :Button = findViewById(R.id.button3)
         diceButton.setOnClickListener{
             dice()
         }
+        diceButton2.setOnClickListener{
+            dice2()
+        }
+        diceTotal.setOnClickListener {
+            total()
+        }
+
+
     }
 
+
     private fun dice() {
-        val randomInt = Random().nextInt(6) + 1
+         randomInt = Random().nextInt(6) + 1
 
         val diceImages = when(randomInt){
             1 -> R.drawable.dice_1
@@ -34,5 +46,29 @@ class MainActivity : AppCompatActivity() {
         }
         val diceImage:ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(diceImages)
+    }
+    private fun dice2() {
+         randomInt1 = Random().nextInt(6) + 1
+
+        val diceImages = when(randomInt1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        val diceImage:ImageView = findViewById(R.id.dice_image2)
+        diceImage.setImageResource(diceImages)
+    }
+
+    private fun total(){
+        var sum:Int = randomInt+randomInt1;
+        var myText:TextView = findViewById(R.id.textView2)
+        myText.text = "total = "+sum.toString();
+
+
+
     }
 }
