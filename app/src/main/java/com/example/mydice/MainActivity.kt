759e40deaae862c42bfcadecda0dc025dabf840c
rishavghosh1331 <54989068+rishavghosh1331@users.noreply.github.com>
2019-11-29 19:28:07 +0530

    completed!

--- app/src/main/java/com/example/mydice/MainActivity.kt
+++ app/src/main/java/com/example/mydice/MainActivity.kt
@@ -9,7 +9,8 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    var randomInt: Int = 0
+    var randomInt1: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,22 +44,34 @@ class MainActivity : AppCompatActivity() {
         diceRollerg.setOnClickListener {
             dice()
         }
+        val diceTotal: Button = findViewById(R.id.niladri)
+        diceTotal.setOnClickListener {
+            dice3()
+        }
     }
 
 
     fun dice() {
-        val randomInt = Random().nextInt(6) + 1
+            val randomInt1 = Random().nextInt(6) + 1
 
-        val diceImages = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
+            val diceImages = when (randomInt1) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.empty_dice
+            }
+            val diceImage: ImageView = findViewById(R.id.dice_nilu)
+            diceImage.setImageResource(diceImages)
         }
-        val diceImage: ImageView = findViewById(R.id.dice_nilu)
-        diceImage.setImageResource(diceImages)
+
+    fun dice3() {
+        var sum: Int = randomInt + randomInt1;
+        var myText:TextView= findViewById(R.id.textView)
+        myText.text= "Total ="+ sum.toString();
+
+
     }
-}
+    }
